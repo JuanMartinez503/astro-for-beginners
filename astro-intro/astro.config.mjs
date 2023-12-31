569e54740c8ac0a62fcc6504cd0674812b90fec6
Juan Martinez <juanmartinez@Juans-iMac.local>
2023-12-31 15:32:48 -0500

    added a few comments and tailwind css to the project

--- astro-intro/astro.config.mjs
+++ astro-intro/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+//adds tailwind by running npx astro add tailwind
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
